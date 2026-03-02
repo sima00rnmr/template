4a38079685aa11e61509c16e396d5adf33ff0a4e
User <User@DESKTOP-B85IULJ>
2026-03-02 23:21:03 +0900

    テンプレ_作成

---
+++ Spring_jpa_template/src/main/java/com/example/hotelleru/SpringJpaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelleru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTemplateApplication.class, args);
+	}
+
+}
---
+++ Spring_jpa_template/src/test/java/com/example/hotelleru/SpringJpaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelleru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
